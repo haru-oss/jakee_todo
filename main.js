133cb9d70f0881ecfd34e8dce8c3af2eaabebf82
haru-oss <hauyaishungry@gmail.com>
2024-04-06 10:41:46 +0900

    IDを取得して、クリックされた時のイベントを書いた

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const onclicked = () => {
-    alert("めちゃくちゃハゲだな");
+
+    // テキストボックスの値を取得して値を初期化する。
+    const inputText = document.getElementById("add-text").value;
+    document.getElementById("add-text").value = ""
 }
+
+// テキストに入力された値を取得して、未完了のTODOのliタグを生成
+
 
 
 document.getElementById("add-todo").addEventListener("click",onclicked);
