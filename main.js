f2c26b9f2e8295a8ab8139c3648a607166a41c62
haru-oss <hauyaishungry@gmail.com>
2024-04-06 17:33:48 +0900

    未完了liタグの　エレメント要素を作成した

--- main.js
+++ main.js
@@ -2,11 +2,36 @@ const onclicked = () => {
 
     // テキストボックスの値を取得して値を初期化する。
     const inputText = document.getElementById("add-text").value;
-    document.getElementById("add-text").value = ""
+        
+    document.getElementById("add-text").value = "";
+    
+    // テキストに入力された値を取得して、未完了のTODOのliタグを生成
+    
+    // li生成
+    const li = document.createElement("li");
+   // li生成 
+    const div = document.createElement("div");
+    div.className = "list-low";
+   
+   // li生成
+   const p = document.createElement("p");
+   p.className = "todo-item";
+   p.innerText = (inputText);
+// innerTextとcontentは何が違うのか？
+
+
+   //    buttun生成
+    const buttun = document.createElement("buttun");
+    buttun.textContent = "完了"
+    
+    
+
+    console.log(p);
+
+
+
+
 }
-
-// テキストに入力された値を取得して、未完了のTODOのliタグを生成
-
 
 
 document.getElementById("add-todo").addEventListener("click",onclicked);
