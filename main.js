5abb54790d506a90887c521f89cb1fafb7a5174d
haru-oss <hauyaishungry@gmail.com>
2024-04-07 17:10:23 +0900

    未完了リストに追加する処理リストに追加する処理を書追加する処理を書いた

--- main.js
+++ main.js
@@ -9,35 +9,39 @@ const onclicked = () => {
     
     // li生成
     const li = document.createElement("li");
-   // li生成 
+    // div.list-low生成 
     const div = document.createElement("div");
     div.className = "list-low";
-   
-   // li生成
-   const p = document.createElement("p");
-   p.className = "todo-item";
-   p.textContent= (inputText);
+    
+    // p.todo-item生成
+    const p = document.createElement("p");
+    p.className = "todo-item";
+    p.textContent= (inputText);
+    // innerTextとcontentは何が違うのか?????????
+    
+    //    buttun生成
+     const buttun = document.createElement("buttun");
+     buttun.textContent = "完了"
+     
+     
+    
+     console.log(p);
+
 
 //    上のタグ達の階層構造を指定していく
-    div.appendChild(p);
     li.appendChild(div);
-    console.log(li);
-// div.appendChild(p)
+    div.appendChild(p);
 
-
-
-
-
-// innerTextとcontentは何が違うのか？
-
-
-   //    buttun生成
-    const buttun = document.createElement("buttun");
-    buttun.textContent = "完了"
-    
+    // 未完了リストに追加
+    document.getElementById("incomplate-list").appendChild(li);
     
 
-    console.log(p);
+
+
+
+
+
+
 
 
 
@@ -51,4 +55,4 @@ document.getElementById("add-todo").addEventListener("click",onclicked);
 
 
 
-// 階層構造まで終わったら、pushする。
+
